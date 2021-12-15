7294a14b1fbf5aac53c037e4bbf94807b367aa57
Siji Jose <sijijosey2015@gmail.com>
2021-12-15 12:16:15 +0530

    5.0

---
+++ WebApplication1/Migrations/20211215064416_migration2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace WebApplication1.Migrations
+{
+    public partial class migration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "adm_number",
+                table: "students",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true)
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "adm_number",
+                table: "students",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+        }
+    }
+}
---
+++ WebApplication1/migration.sql
@@ -0,0 +1,10 @@
+﻿START TRANSACTION;
+
+ALTER TABLE students ALTER COLUMN adm_number 
+DROP IDENTITY;
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20211215064416_migration2', '5.0.0');
+
+COMMIT;
+
