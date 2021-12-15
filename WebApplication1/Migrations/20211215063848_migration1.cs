1bcd9e55699b93295fb0947dc658a2fb98b73a98
Siji Jose <sijijosey2015@gmail.com>
2021-12-15 12:13:46 +0530

    3.1

---
+++ WebApplication1/Migrations/20211215063848_migration1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(nullable: false),
+                    adm_number = table.Column<int>(nullable: true),
+                    name = table.Column<string>(nullable: true),
+                    phone_number = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "students");
+        }
+    }
+}
--- WebApplication1/Student.cs
+++ WebApplication1/Student.cs
@@ -10,44 +10,11 @@ namespace WebApplication1
     public class Student 
     {
         public Guid Id { get; set; }
-        public System.Guid SchoolId { get; set; }
-        public string StudentStatus { get; set; }
-        public string AdmPrefix { get; set; }
+        
         public int? AdmNumber { get; set; }
-        public int AdmNumberIndex { get; set; }
-        public string AdmPostFix { get; set; }
-        public string AdmNo { get; set; }
-        public string TmpPrefix { get; set; }
-        public int? TmpNumber { get; set; }
-        public int TmpNumberIndex { get; set; }
-        public string TmpPostFix { get; set; }
-        public string TmpNo { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public string Name { get; set; }
-        public DateTime? Dob { get; set; }
-        public DateTime? TemporaryJoinDate { get; set; }
-
-        public string Gender { get; set; }         
-        public string UuId { get; set; }
-        public string AcademicYear { get; set; }
-        public string SpecialId { get; set; }
-        public string GuardianName { get; set; }
-        public string FatherName { get; set; }
-        public string MotherName { get; set; }
       
-        public DateTime TerminatedAt { get; set; }
-
-        public Guid? GuardianId { get; set; }
-        public Guid? StudentUserId { get; set; }
-        public string SendUserDetailsVia { get; set; }
-        public string CreateUserNameBy { get; set; }
-        public bool SendUserStatus { get; set; }
-        public int UserPostFixValue { get; set; }
-    
-        public int? WebConnectivityStudentId { get; set; }
-        public DateTime? CreatedAt { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
 
     }
     public class StudentConfiguration : IEntityTypeConfiguration<Student>
@@ -56,50 +23,11 @@ namespace WebApplication1
         {
             builder.ToTable("students");
             builder.Property(h => h.Id).HasColumnName("id");
-            builder.Property(h => h.AdmPrefix).HasColumnName("adm_prefix").HasDefaultValue(null);
-            builder.Property(h => h.AdmNo).HasColumnName("adm_no");
             builder.Property(h => h.AdmNumber).HasColumnName("adm_number").HasDefaultValue(null);
-            builder.Property(h => h.AdmNumberIndex).HasColumnName("adm_number_index");
-            builder.Property(h => h.AdmPostFix).HasColumnName("adm_postfix").HasDefaultValue(null);
-            builder.Property(h => h.TmpPrefix).HasColumnName("tmp_prefix").HasDefaultValue(null);
-            builder.Property(h => h.TmpPostFix).HasColumnName("tmp_postfix").HasDefaultValue(null);
-            builder.Property(h => h.TmpNo).HasColumnName("tmp_no");
-            builder.Property(h => h.TmpNumber).HasColumnName("tmp_number").HasDefaultValue(null);
-            builder.Property(h => h.TmpNumberIndex).HasColumnName("tmp_number_index");
-            builder.Property(h => h.SchoolId).HasColumnName("school_id");
-            builder.Property(h => h.StudentStatus).HasColumnName("student_status");
-            builder.Property(h => h.FirstName).HasColumnName("first_name");
-            builder.Property(h => h.MiddleName).HasColumnName("middle_name");
-            builder.Property(h => h.LastName).HasColumnName("last_name");
             builder.Property(h => h.Name).HasColumnName("name");
-            builder.Property(h => h.Dob).HasColumnName("dob");
-            builder.Property(h => h.Gender).HasColumnName("gender");
-            builder.Property(h => h.UuId).HasColumnName("uu_id");
-            builder.Property(h => h.AcademicYear).HasColumnName("academic_year");
-            builder.Property(h => h.SpecialId).HasColumnName("special_id");
-            builder.Property(h => h.GuardianName).HasColumnName("guardian_name");
-            builder.Property(h => h.FatherName).HasColumnName("father_name");
-            builder.Property(h => h.MotherName).HasColumnName("mother_name");
-            builder.Property(h => h.TerminatedAt).HasColumnName("terminated_at");
-            builder.Property(h => h.GuardianId).HasColumnName("guardian_id");
-            builder.Property(h => h.StudentUserId).HasColumnName("student_user_id ");
-            builder.Property(h => h.TemporaryJoinDate).HasColumnName("temporary_join_date");
-            builder.Property(h => h.SendUserDetailsVia).HasColumnName("send_user_details_via");
-            builder.Property(h => h.CreateUserNameBy).HasColumnName("create_user_name_by");
-            builder.Property(h => h.SendUserStatus).HasColumnName("send_user_status").HasDefaultValue(false);
-            builder.Property(h => h.UserPostFixValue).HasColumnName("user_post_fix_value");
-            builder.Property(h => h.WebConnectivityStudentId).HasColumnName("web_connectivity_student_id");
-            builder.Property(s => s.CreatedAt).HasColumnName("created_at").HasDefaultValueSql("transaction_timestamp()");
+            builder.Property(h => h.PhoneNumber).HasColumnName("phone_number");
 
-            builder.HasIndex(s => new { s.SchoolId, s.AdmNo })
-            .IsUnique().HasName("IX_UNIQ_ADMISSION_NUMBER").HasFilter("adm_no IS NOT NULL");
-            builder.HasIndex(s => new { s.SchoolId, s.TmpNo })
-            .IsUnique().HasName("IX_UNIQ_TEMPORARY_NUMBER").HasFilter("tmp_no IS NOT NULL");
-
-            builder.HasIndex(s => new { s.SchoolId, s.UuId }).IsUnique().HasName("IX_UNIQ_UUID_NUMBER");
-
-            builder.HasIndex(b => new { b.SchoolId, b.WebConnectivityStudentId }).IsUnique()
-           .HasName("IX_UNIQ_WEB_CONNECTIVITY_STUDENT_ID").HasFilter("web_connectivity_student_id IS NOT NULL");
+        
 
         }
     }
